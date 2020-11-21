9bd9fde7c340b8b2607206d9f9d7a28340a8198c
Oleg Kolomiyets <oleg.kolomiets@gmail.com>
2020-11-21 20:06:02 +0200

    made fadeIn, calculating width and height

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 "use strict";
 //option
-let timeout = 10000;
+let timeout = 0;
 let changeTime = 5000;
 
 //
+function showRandomPic() {
+  let random = Math.floor(Math.random() * imagesArr.length);
+  console.log(random);
+  return imagesArr[random];
+}
+
+function random(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
 const imagesArr = [
   "https://images.pexels.com/photos/1275929/pexels-photo-1275929.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=9060",
   "https://images.pexels.com/photos/1451074/pexels-photo-1451074.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=450&w=560",
@@ -13,27 +24,18 @@ const imagesArr = [
   "https://images.pexels.com/photos/885880/pexels-photo-885880.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=150&w=1260",
   "https://images.pexels.com/photos/1112598/pexels-photo-1112598.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
 ];
-let container = document.querySelector(".screensaver");
-
-function showRandomPic() {
-  let random = Math.floor(Math.random() * (imagesArr.length - 0 + 1)) + 0;
-  return imagesArr[random];
-}
-
+let container = document.querySelector("#screensaver");
 let img = document.createElement("span");
-// photo.classList.add("photo");
 
 setInterval(() => {
-  //img.style.left = Math.floor(Math.random() * (max - min + 1)) + min; // сделать нормально
+  let randomPics = showRandomPic();
 
-  //window.innerWidth
-  //window.innerHeight;
-  img.innerHTML = `<img src=${showRandomPic()}>`;
-  img.style.left =
-    Math.floor(Math.random() * (window.innerWidth - 0 + 1)) + 0 + "px";
-  img.style.top =
-    Math.floor(Math.random() * (window.innerHeight - 0 + 1)) + 0 + "px";
+  let height = randomPics.match(/\&h=([\d]+)/);
+  let width = randomPics.match(/\&w=([\d]+)/);
+  img.classList.toggle("fadeMe");
+  img.innerHTML = `<img src=${randomPics} width="${width[1]}" height="${height[1]}">`;
+  img.style.left = `${random(0, window.innerWidth - width[1])}px`;
+  img.style.top = `${random(0, window.innerHeight - height[1])}px`;
 }, changeTime);
 
-// console.log(setInterval);
 container.appendChild(img);
