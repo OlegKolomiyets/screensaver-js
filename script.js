f9ef171966bc0de6bb38645a952f21fba35e1f96
Oleg Kolomiyets <oleg.kolomiets@gmail.com>
2020-11-22 20:05:10 +0200

    working version

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
 "use strict";
 //option
-let timeout = 0;
+let timeout = 10000;
 let changeTime = 5000;
+//
+
+let timeoutID;
+let intervalID;
+
+let root = document.documentElement;
+
+const fadetime = changeTime / 1000 + "s";
+
+function init() {
+  document.addEventListener("mousemove", resetTimer, false);
+  document.addEventListener("click", resetTimer, false);
+  document.addEventListener("mousedown", resetTimer, false);
+  document.addEventListener("keydown", resetTimer, false);
+  document.addEventListener("mousewheel", resetTimer, false);
+
+  startTimer();
+}
+
+init();
+
+function startTimer() {
+  timeoutID = setTimeout(startScreenSaver, timeout);
+}
+
+function resetTimer(e) {
+  clearTimeout(timeoutID);
+  clear();
+}
 
 //
 function showRandomPic() {
   let random = Math.floor(Math.random() * imagesArr.length);
-  console.log(random);
   return imagesArr[random];
 }
 
 function random(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 const imagesArr = [
   "https://images.pexels.com/photos/1275929/pexels-photo-1275929.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=9060",
@@ -24,18 +52,36 @@ const imagesArr = [
   "https://images.pexels.com/photos/885880/pexels-photo-885880.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=150&w=1260",
   "https://images.pexels.com/photos/1112598/pexels-photo-1112598.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
 ];
+
 let container = document.querySelector("#screensaver");
 let img = document.createElement("span");
 
-setInterval(() => {
-  let randomPics = showRandomPic();
+function startScreenSaver() {
+  root.style.setProperty("--fadetime", fadetime);
 
-  let height = randomPics.match(/\&h=([\d]+)/);
-  let width = randomPics.match(/\&w=([\d]+)/);
-  img.classList.toggle("fadeMe");
-  img.innerHTML = `<img src=${randomPics} width="${width[1]}" height="${height[1]}">`;
-  img.style.left = `${random(0, window.innerWidth - width[1])}px`;
-  img.style.top = `${random(0, window.innerHeight - height[1])}px`;
-}, changeTime);
+  intervalID = setInterval(() => {
+    img.className = "fade";
+    let randomPics = showRandomPic();
+    let height = randomPics.match(/\&h=([\d]+)/);
+    let width = randomPics.match(/\&w=([\d]+)/);
+    // img.innerHTML = `<img src=${randomPics}>`;
+    img.innerHTML = `<img src=${randomPics} width="${width[1]}" height="${height[1]}">`;
+    img.style.left = `${random(0, window.innerWidth - width[1])}px`;
+    img.style.top = `${random(0, window.innerHeight - height[1])}px`;
+    container.appendChild(img);
+  }, changeTime);
+}
 
-container.appendChild(img);
+function clearContainer(container) {
+  let child = container.lastElementChild;
+  while (child) {
+    container.removeChild(child);
+    child = container.lastElementChild;
+  }
+}
+
+function clear() {
+  clearContainer(img);
+  clearInterval(intervalID);
+  startTimer();
+}
